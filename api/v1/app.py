5ecdc6604e940d9a1d7282be1c7a2aae8000d92e
Kirito99x <msaifeldin46@gmail.com>
2024-04-26 10:12:42 +0300

    5-notfound

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """flask api-web-application"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -13,6 +13,9 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def shutdown(exception=None):
     storage.close()
 
+@app.errorhandler(404)
+def not_found(exception=None):
+    return jsonify({"error":"Not found"}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', threaded=True)
