6589b3b0e66465ab4544ae4fe15473a369499805
Kirito99x <msaifeldin46@gmail.com>
2024-04-26 09:48:09 +0300

    4-somestats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,22 @@
 """"Index view module"""
 from flask import jsonify
 from . import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Return status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    states = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(states)
