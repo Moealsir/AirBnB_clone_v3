0e36378a4f9290f0820eea7e8499dee933a3d310
Moealsir <mohamedwdalsir@gmail.com>
2024-04-26 18:15:38 +0300

    chmohdhttps://meet.google.com/hse-hqms-ytb

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,13 +9,16 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def shutdown(exception=None):
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(exception=None):
-    return jsonify({"error":"Not found"}), 404
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', threaded=True)
