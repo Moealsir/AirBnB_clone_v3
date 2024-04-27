a9675f8c14a8bcf92ea9c4de449ce2e7e9749a83
Kirito99x <msaifeldin46@gmail.com>
2024-04-27 04:13:44 +0300

    4fix2.1

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,13 +13,19 @@ def status():
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
-    """return the count of stats and jsonify it"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(stats)
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
