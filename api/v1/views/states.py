67550874e83223b79b8dcfb70c7d1c5d70ac0122
Kirito99x <msaifeldin46@gmail.com>
2024-04-27 04:46:46 +0300

    6fix1.0

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""State view module"""
+"""this is the State view module"""
 from flask import jsonify, abort, make_response, request
 from . import app_views
-from models import state
+from models import state, storage
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
-    """ status view function """
-    states = state.State.query.all()
+    """ this is finction status view function """
+    states = storage.all(state).values()
     return jsonify([state.to_dict() for state in states])
