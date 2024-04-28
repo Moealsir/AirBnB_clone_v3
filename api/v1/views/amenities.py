9c0eb2460e7e8b98052f052fd29f29651de4dada
Kirito99x <msaifeldin46@gmail.com>
2024-04-28 12:36:47 +0300

    tasl 8 all_get get_id create are working fix pycodestyle trying checker

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,16 +5,19 @@ from models import storage
 from models.amenity import Amenity
 from . import app_views
 
+
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """return the list of all Amenity objects"""
-    objects_am = storage.all(Amenity).values() 
+    objects_am = storage.all(Amenity).values()
     list_of_am = []
     for am in objects_am:
         list_of_am.append(am.to_dict())
     return jsonify(list_of_am)
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_amenities_id(amenity_id):
     """return the amenity based on the id"""
     am = storage.get(Amenity, amenity_id)
@@ -40,4 +43,3 @@ def create_amenity_id():
     insta = Amenity(**dt)
     insta.save()
     return make_response(jsonify(insta.to_dict()), 201)
-        
