9a36038ee677a63dd55585fe6c5bff0db0ce8ec7
Kirito99x <msaifeldin46@gmail.com>
2024-04-28 17:28:34 +0300

    try in the inedx file it faild

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,25 @@
 #!/usr/bin/python3
-"""
-Flask route that returns json status response
-"""
-from api.v1.views import app_views
-from flask import jsonify, request
-from models import storage
+""""this is the Index view module"""
+from flask import jsonify
+from . import app_views
+from models import storage, state
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """
-    function for status route that returns the status
-    """
-    if request.method == 'GET':
-        resp = {"status": "OK"}
-        return jsonify(resp)
+    """Return status ok"""
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
-    """
-    function to return the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+    """return the count of stats and jsonify it"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
