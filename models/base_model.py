7a791907e3733b0828c559cba83c9361ba08638b
Moealsir <mohamedwdalsir@gmail.com>
2024-04-29 14:21:53 +0300

    fixed model base

--- models/base_model.py
+++ models/base_model.py
@@ -61,11 +61,9 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
-        if "created_at" in new_dict and isinstance(
-             new_dict["created_at"], datetime):
+        if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict and isinstance(
-             new_dict["updated_at"], datetime):
+        if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
